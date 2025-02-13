ea3b5f3630bfac68b4f62d7ae6f7b0d6e433cb01
lascastagneres <cyrille.lequinio@qwanza.fr>
2025-02-13 13:40:25 +0100

    website

---
+++ out/_next/static/chunks/app/[locale]/menu/page-14579d5386c91413.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[145,465],{648:(e,s,n)=>{Promise.resolve().then(n.bind(n,3401)),Promise.resolve().then(n.bind(n,8193))}},e=>{var s=s=>e(e.s=s);e.O(0,[193,441,517,358],()=>s(648)),_N_E=e.O()}]);
